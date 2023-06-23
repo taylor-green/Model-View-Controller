714c76733aa39cce3d9ab7d27e2fcf62e8c8c3d3
taylor-green <taylor.green2804@gmail.com>
2023-06-23 11:10:22 -0400

    update

---
+++ server.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./routes');
+
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 100000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+// Set up session middleware
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+// Set the view engine
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+  extname: 'handlebars',
+}));
+app.set('view engine', 'handlebars');
+
+// Define a route handler for "/login"
+app.get('/login', (req, res) => {
+  res.render('loginForm'); 
+});
+
+// Define a route handler for "/signup"
+app.get('/signup', (req, res) => {
+  res.render('signupForm'); 
+});
+
+
+// Set up static file serving and body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes
+app.use(routes);
+
+// Start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+  });
+});
