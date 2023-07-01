15147000b0f54658c16ad95c464cf9c061cfb806
taylor-green <taylor.green2804@gmail.com>
2023-07-01 09:40:50 -0400

    updated connection.js

--- MVC/config/connection.js
+++ MVC/config/connection.js
@@ -4,7 +4,6 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-// Check if the JAWSDB_URL environment variable is defined
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -14,19 +13,12 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST || 'localhost',
+      host: 'localhost',
       dialect: 'mysql',
-      port: process.env.DB_PORT || 3306,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-      },
-    }
+      port: 3306,
+      }
   );
 }
+  
 // Export the created Sequelize instance for use in other modules
 module.exports = sequelize;
