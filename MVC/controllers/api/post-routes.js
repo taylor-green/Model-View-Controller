6063a57d4bef87379c5fc383c5543cb52784c0e0
taylor-green <taylor.green2804@gmail.com>
2023-07-01 09:57:25 -0400

    refactored post-routes

--- MVC/controllers/api/post-routes.js
+++ MVC/controllers/api/post-routes.js
@@ -1,47 +1,114 @@
-const { Post, User } = require('../models');
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
-const postController = {
-  // Handler for creating a new blog post
-  async createPost(req, res) {
-    try {
-      // Create a new post in the database
-      const post = await Post.create({ ...req.body, userId: req.session.userId });
 
-      // Redirect to the dashboard
-      res.redirect('/dashboard');
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to create blog post' });
-    }
-  },
 
-  // Handler for updating a blog post
-  async updatePost(req, res) {
-    try {
-      // Update the post in the database
-      await Post.update(req.body, { where: { id: req.params.id } });
+router.get('/', async (req, res) => {
+  try {
+    
+    const post = await Post.findAll({
+      include: [{ model: User, attributes: ['name'] }],
+    });
+    const postData = post.map((post) => post.get({ plain: true }));
+    // Render 'posts' template with retrieved posts data
+    res.render('posts', {
+      postData,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-      // Redirect to the dashboard
-      res.redirect('/dashboard');
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to update blog post' });
-    }
-  },
+// Route for updating post by ID
+router.put('/:id', async (req, res) => {
+  try {
+    // Update the post description with the provided ID
+    const post = await Post.update(
+      { description: req.body.description },
+      { where: { id: req.params.id } }
+    );
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-  // Handler for deleting a blog post
-  async deletePost(req, res) {
-    try {
-      // Delete the post from the database
-      await Post.destroy({ where: { id: req.params.id } });
+// Route for creating new post
+router.post('/', async (req, res) => {
+  try {
+ 
+    const post = await Post.create({
+      ...req.body,
+      created_by: req.session.user_id,
+    });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-      // Redirect to the dashboard
-      res.redirect('/dashboard');
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to delete blog post' });
-    }
-  },
-};
+// Route for rendering the comment form for a specific post
+router.get('/:id/comment', async (req, res) => {
+  try {
+    // Retrieve the post with associated user name for the provided ID
+    const post = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ['name'] }],
+    });
+    const postData = post.get({ plain: true });
+    // Render the 'comment' template with the retrieved post data
+    res.render('comment', {
+      ...postData,
+      logged_in: req.session.logged_in,
+      logged_user_name: req.session.user_name,
+      logged_user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-module.exports = postController;
+// Route for rendering a specific post with associated user name and comments
+router.get('/:id', async (req, res) => {
+  try {
+    // Retrieve the post with associated user name and comments for the provided ID
+    const post = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ['name'],
+          },
+        },
+      ],
+    });
+    const postData = post.get({ plain: true });
+    // Render the 'post' template with the retrieved post data
+    res.render('post', {
+      ...postData,
+      logged_in: req.session.logged_in,
+      logged_user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  try {
+    
+    const post = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
