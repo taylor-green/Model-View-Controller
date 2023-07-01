6accad1e256b2f1d9286a6542235da74b229152f
taylor-green <taylor.green2804@gmail.com>
2023-07-01 09:45:07 -0400

    deleted dashboardroutes and reformatted homeroutes

--- MVC/controllers/home-routes.js
+++ MVC/controllers/home-routes.js
@@ -1,21 +1,68 @@
-// homeController.js
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
 
-const { Post, User } = require('../models');
+// Route for the homepage
+router.get('/', async (req, res) => {
+  res.render('homepage', {
+    logged_in: req.session.logged_in
+  });
+});
 
-const homeController = {
-  // Handler for the homepage route
-  async homepage(req, res) {
-    try {
-      // Fetch existing blog posts
-      const posts = await Post.findAll({ include: User });
+// Route for user signup
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-      // Render the homepage view with the posts data
-      res.render('homepage', { posts });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to retrieve blog posts' });
-    }
-  },
-};
+// Route for user dashboard and their posts
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.session.user_id, {
+      include: [
+        { model: Post },
+      ],
+    });
+    const userData = user.get({ plain: true });
+    res.render('dashboard', {
+      ...userData,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = homeController;
+// Route for user login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to another route
+  if (req.session.logged_in) {
+    res.redirect('/api/posts');
+    return;
+  }
+  res.render('login', {
+    is_login: true
+  });
+});
+
+// Route for user signup
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect to another route
+  if (req.session.logged_in) {
+    res.redirect('/api/postRoutes');
+    return;
+  }
+  res.render('login', {
+    is_login: false
+  });
+});
+
+module.exports = router;
